21cc340833176b8517cb6d3b51d34fd4aab4041c
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-10-13 14:39:32 +0100

    Tab integrated. is OK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './assets/css/style.css';
 
 import { initialLoad } from './initial_page_load';
 
 console.log("Here is Restaurange Page");
 initialLoad();
+
+
--- src/initial_page_load.js
+++ src/initial_page_load.js
@@ -1,3 +1,7 @@
+import { homeLoad } from './content/home';
+import { menuLoad } from './content/menu';
+import { contactLoad } from './content/contact';
+
 const initialLoad = () => {
   let content = `
     <div>
@@ -6,44 +10,52 @@ const initialLoad = () => {
     </div>
 
     <div class="tabsy">
-      <input type="radio" id="tab1" name="tab" checked="">
-      <label class="tabButton" for="tab1">Tab One</label>
+      <input type="radio" id="home" name="tab" checked="">
+      <label class="tabButton" for="home">Home</label>
       
-      <div class="tab">
-          <div class="content">
-              <h3>Heading 1</h3>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-          </div>
-      </div>
+      <input type="radio" id="menu" name="tab">
+      <label class="tabButton" for="menu">Menu</label>
+      
 
-      <input type="radio" id="tab2" name="tab">
-      <label class="tabButton" for="tab2">Tab Two</label>
+      <input type="radio" id="contact" name="tab">
+      <label class="tabButton" for="contact">Contact</label>
       
-      <div class="tab">
-        <div class="content">
-            <h3>Heading 2</h3>
-            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-        </div>
-      </div>
 
-      <input type="radio" id="tab3" name="tab">
-      <label class="tabButton" for="tab3">Tab Three</label>
-      
       <div class="tab">
-          <div class="content">
-              <h3>Heading 3</h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          <div class="content" id="tab-content">
+              
           </div>
       </div>
 
     </div>
   `;
   document.getElementById('content').innerHTML = content;
+  homeLoad();
+
+  addListener();
 }
+
+
+const addListener = () => {
+  document.getElementById("content").addEventListener('click', function(e) {
+    switch (e.target.innerHTML) {
+      case 'Home':
+        homeLoad();
+
+        break;
+      case 'Menu':
+        menuLoad();
+
+        break;
+      case 'Contact':
+        contactLoad();
+
+        break;
+      default:
+
+    }
+  });
+};
 
 export {
   initialLoad
